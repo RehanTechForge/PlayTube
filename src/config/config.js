42dadfd0d3f1bb48634fbf9c35355a4f9ed85704
Muhammad Rehan <muhammadrehan125768@gmail.com>
2024-11-17 17:06:21 +0500

    feat: Add dotenv configuration and environment variable support
    - Installed `dotenv` for managing environment variables.
    - Created a `.env.sample` file to serve as a template for environment variables.
    - Updated the configuration file to load environment variables using `dotenv`.
    - Secured sensitive information by recommending `.env` in `.gitignore`.
    
    Notes:
    - Ensure to replace placeholder values in `.env.sample` with actual credentials.
    - Avoid committing the `.env` file to version control.

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,16 @@
 const _config = {
   PORT: process.env.PORT,
   MONGODB_URI: process.env.MONGODB_URI,
-  CORS: process.env.CORS,
+  FRONTEND_URL: process.env.FRONTEND_URL,
+  ENV: process.env.ENV,
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+  DOMAIN: process.env.DOMAIN,
 };
 
 const config = Object.freeze(_config);
